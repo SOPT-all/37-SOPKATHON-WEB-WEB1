ddbb896ac5f808139c06806bc68fe73d2c722f91
odukong <ohbin1017@gmail.com>
2025-11-21 20:10:52 +0900

    feat : 디자인 토큰 시스템 구축

---
+++ src/styles/base/global.css.ts
@@ -0,0 +1,28 @@
+import { globalStyle } from "@vanilla-extract/css";
+
+/** HTML & Body style */
+globalStyle("html, body", {
+  width: "100%",
+  // minHeight: "100vh", (웹앱일 경우 주석 제거)
+  margin: "0",
+  padding: "0",
+  boxSizing: "border-box",
+  scrollBehavior: "smooth",
+  fontSize: "62.5%",
+
+  WebkitFontSmoothing: "antialiased",
+  MozOsxFontSmoothing: "grayscale",
+});
+
+globalStyle("*, *::before, *::after", {
+  boxSizing: "inherit",
+});
+
+/** Scrollbar Hide */
+globalStyle("::-webkit-scrollbar", {
+  display: "none",
+});
+
+globalStyle("*", {
+  WebkitTapHighlightColor: "transparent",
+});
